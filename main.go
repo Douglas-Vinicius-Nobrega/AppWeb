bec1f9b53337386874de9a640813e6688deefd52
Douglas Vinicius Nobrega <dnobregac2@gmail.com>
2022-04-02 14:39:08 -0300

    Estudos dia 02-04

--- main.go
+++ main.go
@@ -5,13 +5,13 @@ import (
 	"text/template"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*html")) // carregar todos os templates, para o serv
+var temp = template.Must(template.ParseGlob("templates/*.html")) // carregar todos os templates, para o serv
 
 func main() {
-	http.HandleFunc("/", index)       // toda vez que tiver uma requisição para a barra, quem vai atender é o index
+	http.HandleFunc("/", index)       // toda vez que tiver uma requisição para a barra(/), quem vai atender é a função index
 	http.ListenAndServe(":8000", nil) // subindo um servido, na porta 8000 (8 mil)
 }
 
-func index(w http.ResponseWriter, r *http.Request) { //
-	temp.ExecuteTemplate(w, "Index", nil)
+func index(w http.ResponseWriter, r *http.Request) { // Toda vez que tiver um requisição, vamos ter esses dois parametros
+	temp.ExecuteTemplate(w, "Index", nil) // executando nosso template de index dentro dessa função
 }
